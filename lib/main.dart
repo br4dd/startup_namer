5957fb6346a616928700c22ade940aea70590d5e
br4dd <earljohn321123@gmail.com>
2021-06-21 11:16:32 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: Row(
+          children: [Text('Hello World')],
+        ),
+      ),
+    );
+  }
+}
